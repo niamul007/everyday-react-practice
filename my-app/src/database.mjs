8886c747658451621e58fc1a784f679f68a583ba
niamul007 <am3994566@gmail.com>
2025-10-26 18:50:18 +0600

    adding initial comment

---
+++ my-app/src/database.mjs
@@ -0,0 +1,23 @@
+const API_KEY = '2237fea974mshb7da06f3304f35dp141c59jsn7d24ae5d9c51';
+async function getData(){
+    try{
+        const response = await fetch('https://imdb236.p.rapidapi.com/api/imdb/cast/nm0000190/titles',
+            {
+                method: 'GET',
+                headers: {
+                    'x-rapidapi-key': API_KEY,
+                    'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+                }
+            }
+        )
+
+        const data = await response.json();
+        console.log(typeof(data));
+
+    }
+    catch(error){
+        console.error(error.message)
+    }
+}
+
+getData();
