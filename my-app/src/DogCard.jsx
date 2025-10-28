5022f8c225a8c2a168de90753a62b885341838fa
niamul007 <am3994566@gmail.com>
2025-10-28 17:34:47 +0600

    adding usestate

--- my-app/src/DogCard.jsx
+++ my-app/src/DogCard.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 export function DogCard(props) {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(count + 1);
+        return count;
+    }
     return(
         <div className="container">
             <img src={props.img} alt={`dog's breed is ${props.breedName}`} className="dog-img" />
             <h2 className="dog-breed">{props.breedName}</h2>
+            <p>count : {count}</p>
+            <button onClick = { handleClick}>Click here</button>
         </div>
     )
 }
