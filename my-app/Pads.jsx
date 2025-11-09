e18695a99fdadb1d99c1e3efd562501ecdaf3099
niamul007 <am3994566@gmail.com>
2025-11-09 20:12:37 +0600

    adding ternay practice

--- my-app/Pads.jsx
+++ my-app/Pads.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Pads(props) {
   return (
-    <button style={{ backgroundColor: `${props.color}` }}>
+    <button style={{ backgroundColor: `${props.color}` }} className={` ${props.on ? "on" : "button"}`}>
     </button>
   );
 }
