120b72fe64d4b32f1e26a135685efde5d807d251
niamul007 <am3994566@gmail.com>
2025-11-18 12:14:33 +0600

    adding new practice to the app

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,13 +3,19 @@ import { Jokes } from "./Jokes.jsx";
 import { data } from "./data.js";
 import Test from "./Test.jsx";
 import Hotel from "./Hotel.jsx";
+import Header from "./Header.jsx";
+import Body from "./Body.jsx";
+
 export function App() {
   const jokesData = data.map((item) => {
     return <Jokes key={item.id} {...item} />;
   });
   return <>
   {/* <Test /> */}
-  <Hotel />
+  {/* <Hotel /> */}
+  <Header />
+  <Body />
+  {/* {jokesData} */}
   </>;
 }
 
