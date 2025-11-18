84dfec3861256494bc90f8593dc034056b73a79a
niamul007 <am3994566@gmail.com>
2025-11-18 12:13:31 +0600

    adding body for it

---
+++ my-app/src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src='https://i.pinimg.com/1200x/7f/0c/b7/7f0cb76867a20c0ee038618be5fa3182.jpg'
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
