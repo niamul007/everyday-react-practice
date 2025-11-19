f3b45252f15a0c12dd6a7ea466a3e5dcc3a2ad34
niamul007 <am3994566@gmail.com>
2025-11-19 14:43:13 +0600

    adding Random img

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,18 +5,21 @@ import Test from "./Test.jsx";
 import Hotel from "./Hotel.jsx";
 import Header from "./Header.jsx";
 import Body from "./Body.jsx";
-
+import Effect from "./Effect.jsx";
 export function App() {
   const jokesData = data.map((item) => {
     return <Jokes key={item.id} {...item} />;
   });
-  return <>
-  {/* <Test /> */}
-  {/* <Hotel /> */}
-  <Header />
-  <Body />
-  {/* {jokesData} */}
-  </>;
+  return (
+    <>
+      {/* <Test /> */}
+      {/* <Hotel /> */}
+      <Header />
+      <Body />
+      {/* {jokesData} */}
+      {/* <Effect /> */}
+    </>
+  );
 }
 
 export default App;
