d8fbb66a615cb2d34004d26abea0ec449f7fa0e1
niamul007 <am3994566@gmail.com>
2025-11-19 13:43:17 +0600

    adding practice with efect

--- my-app/src/Effect.jsx
+++ my-app/src/Effect.jsx
@@ -1,26 +1,25 @@
 import { useState, useEffect } from "react";
-
+import Markdown from "react-markdown";
 export default function Effect() {
   const [data, setData] = useState({});
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("https://api.chucknorris.io/jokes/random")
-      .then(res => res.json())
-      .then(data => setData(data));
-  }, [count]);
+  const [count, setCount] = useState(1);
 
   function handleClick() {
-    setCount(prev => prev + 1);
-    console.log("btn clicked");
+    setCount((prev) => prev + 1);
   }
+
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [count]);
 
   console.log(data);
 
   return (
     <div>
-      <h1>Chuck Norris {count}</h1>
-      <p>{data.value}</p>
+      <h1>Star Wars {count}</h1>
+        <Markdown>{`\`\`\`json${JSON.stringify(data, null, 2)}\`\`\``}</Markdown>
       <button onClick={handleClick}>Add</button>
     </div>
   );
